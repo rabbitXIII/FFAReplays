7fbf31d17ac1fc91b658c2d922b6e52b89f60dc9
Rohit Gopal <rohitgopal@gmail.com>
2012-08-19 12:09:50 -0400

    draft 1 of ffareplays website

---
+++ ci/application/config/email.php
@@ -0,0 +1,24 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Email
+| -------------------------------------------------------------------------
+| This file lets you define parameters for sending emails.
+| Please see the user guide for info:
+|
+|	http://codeigniter.com/user_guide/libraries/email.html
+|
+*/
+$config['mailtype'] = 'html';
+$config['protocol']='smtp';
+$config['smtp_host']='ssl://smtp.googlemail.com';
+$config['smtp_port']='465'; 
+$config['smtp_timeout']='30';
+$config['smtp_user']='wc3ffareplays';
+$config['smtp_pass']='123QWEasdZXC';
+$config['charset']='utf-8';
+$config['newline']="\r\n"; 
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */
---
+++ ci/application/controllers/welcome.php
@@ -0,0 +1,29 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->library('tank_auth');
+	}
+	function index(){
+		$data['base'] = $this->config->item('base_url');
+		$data['css'] = 'styles.css';
+		$data['loginout'] = 'login';
+		if ($this->tank_auth->is_logged_in()) {
+			$data['user_id']	= $this->tank_auth->get_user_id();
+			$data['username']	= $this->tank_auth->get_username();
+			$data['loginout'] = 'logout';
+		}
+		$this->load->model('Upload_model');
+		if($this->input->post('upload')) {
+			$this->Upload_model->do_upload();
+		}
+		$this->load->view('main', $data);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
